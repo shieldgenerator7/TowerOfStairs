bd88b80bdeebc54ef107fa3a06f84d0276a94948
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-06-21 00:14:29 -0400

    Added student

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 50;
+
+    public Rotator stairCase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0)
+        {
+            stairCase.speed = moveSpeed * horizontal;
+        }
+        else
+        {
+            stairCase.speed = 0;
+        }
+    }
+}
